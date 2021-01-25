84f16ee32271f6be9cc285a5dce6bf043d9952ef
kolach <chikolad@gmail.com>
2021-01-25 17:23:30 -0500

    feat: add functions to work with container Env array

--- kubernetes/schema_container.go
+++ kubernetes/schema_container.go
@@ -56,3 +56,138 @@ func ResourcesField() map[string]*schema.Schema {
 		},
 	}
 }
+
+// ContainerEnvFields provides schema definition for container Env array
+func ContainerEnvFields(isUpdatable bool) map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"name": {
+			Type:        schema.TypeString,
+			Required:    true,
+			ForceNew:    !isUpdatable,
+			Description: "Name of the environment variable. Must be a C_IDENTIFIER",
+		},
+		"value": {
+			Type:        schema.TypeString,
+			ForceNew:    !isUpdatable,
+			Optional:    true,
+			Description: `Variable references $(VAR_NAME) are expanded using the previous defined environment variables in the container and any service environment variables. If a variable cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded, regardless of whether the variable exists or not. Defaults to "".`,
+		},
+		"value_from": {
+			Type:        schema.TypeList,
+			Optional:    true,
+			MaxItems:    1,
+			Description: "Source for the environment variable's value",
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"config_map_key_ref": {
+						Type:        schema.TypeList,
+						Optional:    true,
+						MaxItems:    1,
+						Description: "Selects a key of a ConfigMap.",
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								"key": {
+									Type:        schema.TypeString,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Description: "The key to select.",
+								},
+								"name": {
+									Type:        schema.TypeString,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Description: "Name of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+								},
+								"optional": {
+									Type:        schema.TypeBool,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Description: "Specify whether the ConfigMap or its key must be defined.",
+								},
+							},
+						},
+					},
+					"field_ref": {
+						Type:        schema.TypeList,
+						Optional:    true,
+						MaxItems:    1,
+						Description: "Selects a field of the pod: supports metadata.name, metadata.namespace, metadata.labels, metadata.annotations, spec.nodeName, spec.serviceAccountName, status.podIP.",
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								"api_version": {
+									Type:        schema.TypeString,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Default:     "v1",
+									Description: `Version of the schema the FieldPath is written in terms of, defaults to "v1".`,
+								},
+								"field_path": {
+									Type:        schema.TypeString,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Description: "Path of the field to select in the specified API version",
+								},
+							},
+						},
+					},
+					"resource_field_ref": {
+						Type:        schema.TypeList,
+						Optional:    true,
+						MaxItems:    1,
+						Description: "Selects a resource of the container: only resources limits and requests (limits.cpu, limits.memory, limits.ephemeral-storage, requests.cpu, requests.memory and requests.ephemeral-storage) are currently supported.",
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								"container_name": {
+									Type:     schema.TypeString,
+									Optional: true,
+									ForceNew: !isUpdatable,
+								},
+								"divisor": {
+									Type:             schema.TypeString,
+									Optional:         true,
+									Default:          "1",
+									ValidateFunc:     ValidateResourceQuantity,
+									DiffSuppressFunc: suppressEquivalentResourceQuantity,
+								},
+								"resource": {
+									Type:        schema.TypeString,
+									Required:    true,
+									ForceNew:    !isUpdatable,
+									Description: "Resource to select",
+								},
+							},
+						},
+					},
+					"secret_key_ref": {
+						Type:        schema.TypeList,
+						Optional:    true,
+						MaxItems:    1,
+						Description: "Selects a key of a secret in the pod's namespace.",
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								"key": {
+									Type:        schema.TypeString,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Description: "The key of the secret to select from. Must be a valid secret key.",
+								},
+								"name": {
+									Type:        schema.TypeString,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Description: "Name of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+								},
+								"optional": {
+									Type:        schema.TypeBool,
+									Optional:    true,
+									ForceNew:    !isUpdatable,
+									Description: "Specify whether the Secret or its key must be defined.",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
--- kubernetes/structures_container.go
+++ kubernetes/structures_container.go
@@ -92,7 +92,131 @@ func ExpandSecretKeyRef(r []interface{}) *v1.SecretKeySelector {
 	if v, ok := in["name"].(string); ok {
 		obj.Name = v
 	}
+	if v, ok := in["optional"]; ok {
+		obj.Optional = ptrToBool(v.(bool))
+	}
 	return obj
+}
+
+// ExpandContainerEnv expands container envs
+func ExpandContainerEnv(in []interface{}) ([]v1.EnvVar, error) {
+	if len(in) == 0 {
+		return []v1.EnvVar{}, nil
+	}
+	envs := make([]v1.EnvVar, len(in))
+	for i, c := range in {
+		p := c.(map[string]interface{})
+		if name, ok := p["name"]; ok {
+			envs[i].Name = name.(string)
+		}
+		if value, ok := p["value"]; ok {
+			envs[i].Value = value.(string)
+		}
+		if v, ok := p["value_from"].([]interface{}); ok && len(v) > 0 {
+			var err error
+			envs[i].ValueFrom, err = ExpandEnvValueFrom(v)
+			if err != nil {
+				return envs, err
+			}
+		}
+	}
+	return envs, nil
+}
+
+// ExpandEnvValueFrom expands env value from diferent sources
+func ExpandEnvValueFrom(r []interface{}) (*v1.EnvVarSource, error) {
+	if len(r) == 0 || r[0] == nil {
+		return &v1.EnvVarSource{}, nil
+	}
+	in := r[0].(map[string]interface{})
+	obj := &v1.EnvVarSource{}
+
+	var err error
+	if v, ok := in["config_map_key_ref"].([]interface{}); ok && len(v) > 0 {
+		obj.ConfigMapKeyRef, err = ExpandConfigMapKeyRef(v)
+		if err != nil {
+			return obj, err
+		}
+	}
+	if v, ok := in["field_ref"].([]interface{}); ok && len(v) > 0 {
+		obj.FieldRef, err = ExpandFieldRef(v)
+		if err != nil {
+			return obj, err
+		}
+	}
+	if v, ok := in["secret_key_ref"].([]interface{}); ok && len(v) > 0 {
+		obj.SecretKeyRef = ExpandSecretKeyRef(v)
+	}
+	if v, ok := in["resource_field_ref"].([]interface{}); ok && len(v) > 0 {
+		obj.ResourceFieldRef, err = ExpandResourceFieldRef(v)
+		if err != nil {
+			return obj, err
+		}
+	}
+	return obj, nil
+
+}
+
+// ExpandConfigMapKeyRef exapnds env value from config map reference
+func ExpandConfigMapKeyRef(r []interface{}) (*v1.ConfigMapKeySelector, error) {
+	if len(r) == 0 || r[0] == nil {
+		return &v1.ConfigMapKeySelector{}, nil
+	}
+	in := r[0].(map[string]interface{})
+	obj := &v1.ConfigMapKeySelector{}
+
+	if v, ok := in["key"].(string); ok {
+		obj.Key = v
+	}
+	if v, ok := in["name"].(string); ok {
+		obj.Name = v
+	}
+	if v, ok := in["optional"]; ok {
+		obj.Optional = ptrToBool(v.(bool))
+	}
+	return obj, nil
+
+}
+
+// ExpandFieldRef expands env value from field reference
+func ExpandFieldRef(r []interface{}) (*v1.ObjectFieldSelector, error) {
+	if len(r) == 0 || r[0] == nil {
+		return &v1.ObjectFieldSelector{}, nil
+	}
+	in := r[0].(map[string]interface{})
+	obj := &v1.ObjectFieldSelector{}
+
+	if v, ok := in["api_version"].(string); ok {
+		obj.APIVersion = v
+	}
+	if v, ok := in["field_path"].(string); ok {
+		obj.FieldPath = v
+	}
+	return obj, nil
+}
+
+// ExpandResourceFieldRef expands env value from resource field reference
+func ExpandResourceFieldRef(r []interface{}) (*v1.ResourceFieldSelector, error) {
+	if len(r) == 0 || r[0] == nil {
+		return &v1.ResourceFieldSelector{}, nil
+	}
+	in := r[0].(map[string]interface{})
+	obj := &v1.ResourceFieldSelector{}
+
+	if v, ok := in["container_name"].(string); ok {
+		obj.ContainerName = v
+	}
+	if v, ok := in["resource"].(string); ok {
+		obj.Resource = v
+	}
+	if v, ok := in["divisor"].(string); ok {
+		q, err := resource.ParseQuantity(v)
+		if err != nil {
+			return obj, err
+		}
+		obj.Divisor = q
+	}
+	return obj, nil
 }
 
 // FlattenResourceList converts resource list into terraform list
@@ -125,6 +249,93 @@ func FlattenSecretKeyRef(in *v1.SecretKeySelector) []interface{} {
 	}
 	if in.Name != "" {
 		att["name"] = in.Name
+	}
+	if in.Optional != nil {
+		att["optional"] = *in.Optional
+	}
+	return []interface{}{att}
+}
+
+// FlattenContainerEnvs translate env vars to terraform structures
+func FlattenContainerEnvs(in []v1.EnvVar) []interface{} {
+	att := make([]interface{}, len(in))
+	for i, v := range in {
+		m := map[string]interface{}{}
+		if v.Name != "" {
+			m["name"] = v.Name
+		}
+		if v.Value != "" {
+			m["value"] = v.Value
+		}
+		if v.ValueFrom != nil {
+			m["value_from"] = FlattenValueFrom(v.ValueFrom)
+		}
+
+		att[i] = m
+	}
+	return att
+}
+
+// FlattenValueFrom converts env value from different sources to terraform structures
+func FlattenValueFrom(in *v1.EnvVarSource) []interface{} {
+	att := make(map[string]interface{})
+
+	if in.ConfigMapKeyRef != nil {
+		att["config_map_key_ref"] = FlattenConfigMapKeyRef(in.ConfigMapKeyRef)
+	}
+	if in.ResourceFieldRef != nil {
+		att["resource_field_ref"] = FlattenResourceFieldSelector(in.ResourceFieldRef)
+	}
+	if in.SecretKeyRef != nil {
+		att["secret_key_ref"] = FlattenSecretKeyRef(in.SecretKeyRef)
+	}
+	if in.FieldRef != nil {
+		att["field_ref"] = FlattenObjectFieldSelector(in.FieldRef)
+	}
+	return []interface{}{att}
+}
+
+// FlattenConfigMapKeyRef converts config map key selector into terraform structures
+func FlattenConfigMapKeyRef(in *v1.ConfigMapKeySelector) []interface{} {
+	att := make(map[string]interface{})
+
+	if in.Key != "" {
+		att["key"] = in.Key
+	}
+	if in.Name != "" {
+		att["name"] = in.Name
+	}
+	if in.Optional != nil {
+		att["optional"] = *in.Optional
+	}
+	return []interface{}{att}
+}
+
+// FlattenResourceFieldSelector converts resource field selector into terraform structures
+func FlattenResourceFieldSelector(in *v1.ResourceFieldSelector) []interface{} {
+	att := make(map[string]interface{})
+
+	if in.ContainerName != "" {
+		att["container_name"] = in.ContainerName
+	}
+	if in.Resource != "" {
+		att["resource"] = in.Resource
+	}
+	if in.Divisor.String() != "" {
+		att["divisor"] = in.Divisor.String()
+	}
+	return []interface{}{att}
+}
+
+// FlattenObjectFieldSelector converts object field selectr into terraform structures
+func FlattenObjectFieldSelector(in *v1.ObjectFieldSelector) []interface{} {
+	att := make(map[string]interface{})
+
+	if in.APIVersion != "" {
+		att["api_version"] = in.APIVersion
+	}
+	if in.FieldPath != "" {
+		att["field_path"] = in.FieldPath
 	}
 	return []interface{}{att}
 }
